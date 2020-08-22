bfb4c35d0408e37326d07b5cfdff5506e0fe5f04
TatianaKon <kontan2025@gmail.com>
2020-08-22 23:58:58 +0300

    1

--- ST/Ramazan Ittiev/Project/src/index.js
+++
@@ -1,7 +0,0 @@
-import bootstrap from '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import './css/styles.css';
-
-import app from './js/main.js';
-
-app();
--- ST/dmitrii-zozulin/Project/src/index.js
+++
@@ -1,9 +0,0 @@
-import jquery from 'jquery';
-import popper from 'popper.js';
-import bootstrap from 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './layout/styles/styles.css';
-
-import app from './components';
-
-app();
